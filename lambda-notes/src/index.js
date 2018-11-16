9647b3c25be74f763f5dba1e818d3b00a322f37b
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2018-11-16 08:51:19 -0600

    added function to add ellipses to end of notes that are too long. Need to add this to my master branch

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
 
 ReactDOM.render(
   <Router>
